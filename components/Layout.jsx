ca7e7cc562f0d066f93b641e88c43613e8669e40
Zer02 <abasdeo29@gmail.com>
2022-10-29 01:42:41 -0400

    Updated frontend components

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>RICO.work</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-sontainer'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
